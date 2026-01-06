3adaed7824545d0fce549b027bfe3bf631565677
redonbrovina <110196643+redonbrovina@users.noreply.github.com>
2026-01-06 22:17:13 +0100

    Revert "Frontend Changes / Homepage Added (#9)"
    This reverts commit e5fc371330e870e5bdaff60fc3fe7169e7b7ba71.
    
    Revert changes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -20,7 +20,6 @@ function App() {
           {/* Client routes */}
           <Route element={<Layout />}>
             <Route index element={<Home />} />
-            <Route path="/home" element={<Home />} /> 
             <Route path="/rreth-nesh" element={<About />} />
             <Route path="/contact" element={<Contact />} />
             <Route path="/sherbimet" element={<Sherbimet />} />
--- frontend/src/pages/client/clientComponents/Img.jsx
+++
@@ -1,8 +0,0 @@
-export default function Img({classNaming, imgSource, description}) {
-    return (
-             <div className={classNaming}>
-                        <img src={imgSource} alt="" />
-                        <p>{description}</p>
-                    </div>
-    );
-}
